a796d7ea1ebae890ae080610e12edda01e0dfd98
Kgotta-contribute <daksh24kumar@gmail.com>
2023-07-10 20:18:21 +0530

    Get your answer here rather than downloading or viewing everything

---
+++ Best Time To Buy & Sell Stocks/Solution.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Solution 
+{
+    public static int maxProfit(int[] prices) 
+    {
+        if (prices.length == 0)
+            return 0;
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int price : prices) 					//This starts a loop that iterates over each element price in the prices array.
+        {
+            minPrice = Math.min(minPrice, price);
+            int currentProfit = price - minPrice;
+            maxProfit = Math.max(maxProfit, currentProfit);
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter the number of days: ");
+        int n = s.nextInt();
+
+        int prices[] = new int[n];
+        System.out.println("Enter the stock prices for each day:");
+        for (int i = 0; i < n; i++) 
+        {
+            prices[i] = s.nextInt();
+        }
+
+        s.close();
+
+        int maxProfit = maxProfit(prices);
+        System.out.println("Maximum Profit: " + maxProfit);
+    }
+}
+
