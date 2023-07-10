341b432d656521a8545ed1377c3e027673077db0
Kgotta-contribute <daksh24kumar@gmail.com>
2023-07-11 01:20:19 +0530

    cpp contains solution

---
+++ Move-Zeroes/mainCPP.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+         int count = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+    for (int i = count; i < nums.size(); i++) {
+        nums[i] = 0;
+    }
+        
+    }
+};
+//Hope it Helps !!
