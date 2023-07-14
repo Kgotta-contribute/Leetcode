7446a821126aa83ec376f6c77e64b2943500b4de
Kgotta-contribute <daksh24kumar@gmail.com>
2023-07-14 15:08:59 +0530

    Solutions commit again changed lil stuff

--- Strivers - Union of Two Sorted Arrays/Solution.cpp
+++ Strivers - Union of Two Sorted Arrays/Solution.cpp
@@ -1,47 +1,49 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
-  int i = 0, j = 0; // pointers
-  vector < int > Union; // Uninon vector
-  while (i < n && j < m) {
-    if (arr1[i] <= arr2[j]) // Case 1 and 2
+vector<int> FindUnion(int a1[], int a2[], int n, int m) 
+{
+  int i = 0, j = 0; 
+  vector< int > UnionArr; 
+  while (i < n && j < m) 
+  {
+    if (a1[i] <= a2[j]) 
     {
-      if (Union.size() == 0 || Union.back() != arr1[i])
-        Union.push_back(arr1[i]);
+      if (UnionArr.size() == 0 || UnionArr.back() != a1[i])
+        UnionArr.push_back(a1[i]);
       i++;
-    } else // case 3
+    } 
+    else 
     {
-      if (Union.size() == 0 || Union.back() != arr2[j])
-        Union.push_back(arr2[j]);
+      if (UnionArr.size() == 0 || UnionArr.back() != a2[j])
+        UnionArr.push_back(a2[j]);
       j++;
     }
   }
-  while (i < n) // IF any element left in arr1
+  while (i < n) // Any element left in array1
   {
-    if (Union.back() != arr1[i])
-      Union.push_back(arr1[i]);
+    if (UnionArr.back() != a1[i])
+      UnionArr.push_back(a1[i]);
     i++;
   }
-  while (j < m) // If any elements left in arr2
+  while (j < m) // If any element left in arr 2
   {
-    if (Union.back() != arr2[j])
-      Union.push_back(arr2[j]);
+    if (UnionArr.back() != a2[j])
+      UnionArr.push_back(a2[j]);
     j++;
   }
-  return Union;
+  return UnionArr;
 }
 
 int main()
 
 {
   int n = 10, m = 7;
-  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  int arr2[] = {2, 3, 4, 4, 5, 11, 12}; 
-  vector < int > Union = FindUnion(arr1, arr2, n, m);
-  cout << "Union of arr1 and arr2 is  " << endl;
-  for (auto & val: Union)
+  int a1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int a2[] = {2, 3, 4, 4, 5, 11, 12}; 
+  vector < int > Union = FindUnion(a1, a2, n, m);
+  cout << "Union of Array1 & Array2 is  " << endl;
+  for (auto &val: Union)
     cout << val << " ";
   return 0;
 }
