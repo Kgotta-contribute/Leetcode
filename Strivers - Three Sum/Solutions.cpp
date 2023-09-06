445ddd573c49f9a4b9b342ae0b450e1bafbe9bb8
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2023-09-06 21:25:19 +0530

    Updated

--- Strivers - Three Sum/Solutions.cpp
+++ Strivers - Three Sum/Solutions.cpp
@@ -61,3 +61,54 @@ int main() {
     
     return 0;
 }
+
+
+
+// Input Vector: nums = {-1, 0, 1, 2, -1, -4}
+
+// Sort the Input Array:
+
+// The input vector nums is sorted in ascending order.
+// After sorting, nums = {-4, -1, -1, 0, 1, 2}.
+// Outer Loop (i):
+
+// The outer for loop iterates through the sorted array from index 0 to n - 2, where n is the size of the array.
+
+// It starts with i = 0 and checks each element in the array.
+
+// Iteration 1 (i = 0):
+
+// i = 0, nums[i] = -4.
+// Inner while loop:
+// left = 1, right = 5.
+// sum = -4 + -1 + 2 = -3 (sum is less than 0, so left is incremented).
+// left = 2, right = 5.
+// sum = -4 + -1 + 1 = -4 (sum is less than 0, so left is incremented).
+// left = 3, right = 5.
+// sum = -4 + -1 + 0 = -5 (sum is less than 0, so left is incremented).
+// left = 4, right = 5.
+// sum = -4 + -1 + -1 = -6 (sum is less than 0, so left is incremented).
+// left and right cannot be moved further as left < right is no longer true.
+// No valid triplet found for i = 0.
+// Iteration 2 (i = 1):
+
+// i = 1, nums[i] = -1.
+// Inner while loop:
+// left = 2, right = 5.
+// sum = -1 + -1 + 2 = 0 (sum equals 0, a valid triplet is found).
+// Triplet { -1, -1, 2 } is added to the result.
+// Duplicates are skipped, and left and right are updated.
+// left = 3, right = 4.
+// sum = -1 + 0 + 1 = 0 (sum equals 0, another valid triplet is found).
+// Triplet { -1, 0, 1 } is added to the result.
+// Iteration 3 (i = 4):
+
+// i = 4, nums[i] = 1.
+// Inner while loop:
+// left = 5, right = 5.
+// sum = 1 + 2 + 2 = 5 (sum is greater than 0, so right is decremented).
+// left and right cannot be moved further as left < right is no longer true.
+// No valid triplet found for i = 4.
+// Result:
+
+// The result vector contains two unique triplets: { -1, -1, 2 } and { -1, 0, 1 }.
