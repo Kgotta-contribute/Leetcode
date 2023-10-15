b2dc5f3bb7b13eebfb3d18944429521d95d847da
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2023-10-15 16:24:12 +0530

    revised & added lines to understand

--- Archy - Add Binary/Solutions.cpp
+++ Archy - Add Binary/Solutions.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
+ // little complex because needs more smartness for being efficient otherwise easy
 class Solution {
 public:
     string addBinary(string a, string b) {
         string result;
         int carry = 0;
-        int i = a.length() - 1;
-        int j = b.length() - 1;
+        int i = a.length() - 1; // 1
+        int j = b.length() - 1;  //0
         
         while (i >= 0 || j >= 0 || carry > 0) {
             int sum = carry;
             
+            // If you want to convert a character to a integer then use this below line.
             if (i >= 0) {
-                sum += a[i] - '0';
+                // sum is now (0 + '1' - '0') = 1, and i is decremented (i=0)
+                sum += a[i] - '0';  //if we have '0' the sum = 48-48 = 0 & if we have '1' the sum = 49-48 = 1 to convert into an integer.
                 i--;
             }
             
             if (j >= 0) {
+                // sum is now (1 + '1' - '0') = 2, and j is decremented (j=-1)
                 sum += b[j] - '0';
                 j--;
             }
             
-            result += to_string(sum % 2);
-            carry = sum / 2;
+            result += to_string(sum % 2); // Append the remainder of sum / 2 (2 % 2 = 0) to result
+            carry = sum / 2;  // Update carry to the integer division of sum / 2 (2 / 2 = 1)
         }
         
-        reverse(result.begin(), result.end());
+        reverse(result.begin(), result.end()); // Reverse the result (0)
         
         return result;
     }
