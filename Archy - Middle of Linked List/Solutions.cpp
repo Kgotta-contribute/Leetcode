ad2f100df1c2ab643ade3a12ded70556e63a8672
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2023-11-20 23:36:13 +0530

    Solutions.cpp

--- Archy - Middle of Linked List/Solutions.cpp
+++ Archy - Middle of Linked List/Solutions.cpp
@@ -37,3 +37,62 @@ int main() {
     
     return 0;
 }
+
+
+
+
+
+
+
+
+
+// JAVA
+
+class ListNode {
+    int val;
+    ListNode next;
+    
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+    
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+    
+    ListNode(int x, ListNode next) {
+        val = x;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        
+        Solution solution = new Solution();
+        ListNode middle = solution.middleNode(head);
+        
+        System.out.println("Middle node value: " + middle.val);
+    }
+}
