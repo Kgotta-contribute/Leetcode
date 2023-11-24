c3a4c0090f53427bf7ce6abc8de8fe610a21afd8
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2023-11-24 07:15:04 +0530

    Java + c++

--- Strivers - Upper Bound/Solutions.cpp
+++ Strivers - Upper Bound/Solutions.cpp
@@ -5,7 +5,6 @@ int upperBound(vector<int> &arr, int x, int n)
 {
     int low = 0, high = n - 1;
     int ans = n;
-
     while (low <= high) 
     {
         int mid = (low + high) / 2;
@@ -29,4 +28,38 @@ int main()
     int ind = upperBound(arr, x, n);
     cout << "The upper bound is the index: " << ind << "\n";
     return 0;
+}
+
+
+
+
+
+// JAVA
+
+import java.util.*;
+
+public class Main {
+    public static int upperBound(List<Integer> arr, int x, int n) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr.get(mid) > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> arr = Arrays.asList(3, 5, 8, 9, 15, 19);
+        int n = 6, x = 9;
+        int ind = upperBound(arr, x, n);
+        System.out.println("The upper bound is the index: " + ind);
+    }
 }
