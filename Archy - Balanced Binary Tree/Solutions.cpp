4d76af0a857613fa87cd1c715cd45ab9a925ca0f
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2023-12-04 13:22:18 +0530

    Java + C++

--- Archy - Balanced Binary Tree/Solutions.cpp
+++ Archy - Balanced Binary Tree/Solutions.cpp
@@ -3,17 +3,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// HEIGHT OF LEFT SUBTREE - RIGHT SUBTREE <= 1 ;
-// Definition for a binary tree node.
+// HEIGHT OF LEFT SUBTREE - RIGHT SUBTREE <= 1 ;       // Definition for a binary tree node.
 struct TreeNode {
     int val;
-    TreeNode* left;
-    TreeNode* right;
+    TreeNode* left; TreeNode* right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 };
-
 class Solution {
 public:
     bool isBalanced(TreeNode* root) 
@@ -21,7 +18,6 @@ public:
         int height = 0; // Variable to store the height difference
         return isBalancedHelper(root, height);
     }
-    
     bool isBalancedHelper(TreeNode* root, int& height)
     {
         if (root == nullptr)
@@ -29,34 +25,26 @@ public:
             height = 0; // Height of an empty tree is 0
             return true;
         }
-        
         int leftHeight = 0, rightHeight = 0;
-        
         // Check if the left subtree is balanced
         bool leftBalanced = isBalancedHelper(root->left, leftHeight);
-        
         if (!leftBalanced)
         {
             return false; // Left subtree is unbalanced, no need to continue
         }
-        
         // Check if the right subtree is balanced
         bool rightBalanced = isBalancedHelper(root->right, rightHeight);
-        
         if (!rightBalanced)
         {
             return false; // Right subtree is unbalanced, no need to continue
         }
-        
         // Calculate the height of the current node's subtree
         height = 1 + max(leftHeight, rightHeight);
-        
         // Check if the current node is balanced
         if (abs(leftHeight - rightHeight) <= 1)
         {
             return true;
         }
-        
         return false;
     }
 };
@@ -92,97 +80,64 @@ int main() {
     return 0;
 }
 
+/*
+          1
+    |           |
+    2           2
+|       |     |      |
+3       3     4      4
 
-
+*/
 
 // Iteration 1:
-
 // root points to node with value 1.
-
 // Left subtree (root->left) is an empty tree.
-
 // Right subtree (root->right) points to node with value 2.
-
 // Iteration 1.1 (right subtree):
-
 // root points to node with value 2.
-
 // Left subtree (root->left) points to node with value 3.
-
 // Right subtree (root->right) points to node with value 3.
-
 // Iteration 1.1.1 (left subtree of right subtree):
-
 // root points to node with value 3.
-
 // Left subtree (root->left) points to node with value 4.
-
 // Right subtree (root->right) points to node with value 4.
-
 // Iteration 1.1.1.1 (left subtree of left subtree of right subtree):
-
 // root points to node with value 4.
 // Left subtree is an empty tree.
 // Right subtree is an empty tree.
 // The height of this subtree is 0.
 // Iteration 1.1.1.2 (right subtree of left subtree of right subtree):
-
 // root points to node with value 4.
 // Left subtree is an empty tree.
 // Right subtree is an empty tree.
 // The height of this subtree is 0.
+
 // Calculate the height of the current node's subtree: height = 1 + max(0, 0) = 1.
-
 // Check if the current node is balanced: abs(0 - 0) <= 1, which is true.
-
 // Calculate the height of the current node's subtree: height = 1 + max(1, 1) = 2.
-
 // Check if the current node is balanced: abs(1 - 1) <= 1, which is true.
-
 // Calculate the height of the current node's subtree: height = 1 + max(2, 2) = 3.
-
 // Check if the current node is balanced: abs(2 - 2) <= 1, which is true.
 
 // Iteration 2:
-
 // root points to node with value 1.
-
 // Left subtree (root->left) is an empty tree.
-
 // Right subtree (root->right) points to node with value 2.
-
 // Iteration 2.1 (right subtree):
-
 // root points to node with value 2.
-
 // Left subtree (root->left) points to node with value 3.
-
 // Right subtree (root->right) points to node with value 3.
-
 // ... (Same as Iteration 1.1)
-
 // Calculate the height of the current node's subtree: height = 1 + max(3, 3) = 4.
-
 // Check if the current node is balanced: abs(3 - 3) <= 1, which is true.
-
 // Iteration 3:
-
 // root points to node with value 1.
-
 // Left subtree (root->left) is an empty tree.
-
 // Right subtree (root->right) points to node with value 2.
-
 // Iteration 3.1 (right subtree):
-
 // root points to node with value 2.
-
 // Left subtree (root->left) points to node with value 3.
-
 // Right subtree (root->right) points to node with value 3.
-
 // ... (Same as Iteration 1.1)
-
 // Calculate the height of the current node's subtree: height = 1 + max(4, 4) = 5.
-
 // Check if the current node is balanced: abs(4 - 4) <= 1, which is true.
--- Archy - Binary Search/Solutions.cpp
+++ Archy - Binary Search/Solutions.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
 class Solution {
 public:
-    int search(std::vector<int>& nums, int target) {
+    int search(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
@@ -18,25 +18,63 @@ public:
                 right = mid - 1; // Adjust the right boundary.
             }
         }
-
         return -1; // Target not found in the array.
     }
 };
 
 int main() {
     Solution solution;
-    std::vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
     int target1 = 9;
     int target2 = 2;
-
     int result1 = solution.search(nums, target1);
     int result2 = solution.search(nums, target2);
-
-    std::cout << "Result 1: " << result1 << std::endl; // Output: 4
-    std::cout << "Result 2: " << result2 << std::endl; // Output: -1
-
+    cout << "Result 1: " << result1 << endl; // Output: 4
+    cout << "Result 2: " << result2 << endl; // Output: -1
     return 0;
 }
+
+
+
+
+// JAVA
+
+import java.util.Arrays;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid; // Target found, return its index.
+            } else if (nums[mid] < target) {
+                left = mid + 1; // Adjust the left boundary.
+            } else {
+                right = mid - 1; // Adjust the right boundary.
+            }
+        }
+        return -1; // Target not found in the array.
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target1 = 9;
+        int target2 = 2;
+        int result1 = solution.search(nums, target1);
+        int result2 = solution.search(nums, target2);
+        System.out.println("Result 1: " + result1); // Output: 4
+        System.out.println("Result 2: " + result2); // Output: -1
+    }
+}
+
+
+
 
 
 
