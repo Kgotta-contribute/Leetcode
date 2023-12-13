ad13959b149d3936b22c593d4789f58d65c3acf9
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2023-12-13 22:52:30 +0530

    JAVA + c++

--- Archy - Coin Change/Solutions.cpp
+++ Archy - Coin Change/Solutions.cpp
@@ -42,3 +42,64 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+// JAVA
+
+
+
+
+
+
+
+
+
+import java.util.Arrays;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0; // 0 coins needed to make 0 amount
+
+        for (int i = 1; i <= amount; ++i) {
+            for (int coin : coins) {
+                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Example 1
+        int[] coins1 = {1, 2, 5};
+        int amount1 = 11;
+        System.out.println("Example 1: " + solution.coinChange(coins1, amount1)); // Output: 3
+
+        // Example 2
+        int[] coins2 = {2};
+        int amount2 = 3;
+        System.out.println("Example 2: " + solution.coinChange(coins2, amount2)); // Output: -1
+
+        // Example 3
+        int[] coins3 = {1};
+        int amount3 = 0;
+        System.out.println("Example 3: " + solution.coinChange(coins3, amount3)); // Output: 0
+    }
+}
+
+
+
+
