47909dd595673fcef747011eac5360138523096c
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2023-12-16 23:33:37 +0530

    Java + C++

--- COLLEGE/CommonElementsof3sortedarrays.cpp
+++ COLLEGE/CommonElementsof3sortedarrays.cpp
@@ -23,3 +23,29 @@ void findInter(int a1[], int a2[], int temp[], int m, int n, int k)
         }
     }
 }
+
+
+
+
+// JAVA
+
+
+
+
+public class Main {
+    public static void findInter(int[] a1, int[] a2, int[] temp, int m, int n, int k) {
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (a1[i] < a2[j]) {
+                i++;
+            } else if (a2[j] < a1[i]) {
+                j++;
+            } else {
+                temp[k] = a1[i];
+                i++;
+                j++;
+                k++;
+            }
+        }
+    }
+}
