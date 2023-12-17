97d4ba8b5a4fb4bfadf7a874dde0d643c48ec477
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2023-12-17 15:50:04 +0530

    Java + C++

---
+++ COLLEGE recommended CODE for everyone CIMP/MinSwaps&Ktogether.cpp
@@ -0,0 +1,63 @@
+// ON GFG
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minSwap(int arr[], int n, int k) {
+        // Complet the function
+        int count = 0;
+    for (int i = 0; i < n; i++) {
+        //This loop iterates through the array and increments the "count" for each element less than or equal to k.
+        if (arr[i] <= k) {
+            count++;
+        }
+    }
+
+    //This will be used to count the number of elements greater than k.
+    int outOfRange = 0;
+    for (int i = 0; i < count; i++) {
+        //This loop iterates through the first "count" elements of the array and increments "outOfRange" for each element greater than k.
+        if (arr[i] > k) {
+            //represents the number of elements greater than k within the first "count" elements of the array.
+            outOfRange++;
+        }
+    }
+
+    return outOfRange;
+    }
+};
+
+
+
+
+// java
+
+
+
+
+
+
+
+public class Solution {
+    public int minSwap(int[] arr, int n, int k) {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= k) {
+                count++;
+            }
+        }
+
+        int outOfRange = 0;
+        for (int i = 0; i < count; i++) {
+            if (arr[i] > k) {
+                outOfRange++;
+            }
+        }
+
+        return outOfRange;
+    }
+}
+
