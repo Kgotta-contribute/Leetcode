7499d6b92c3414ba734c4bd5c569577b8debc5b6
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2024-01-15 23:38:02 +0530

    java + c++

--- Strivers - Floor & Ceil/Solutions.cpp
+++ Strivers - Floor & Ceil/Solutions.cpp
@@ -5,7 +5,7 @@ int findFloor(int arr[], int n, int x)
 {
     int low = 0, high = n-1, ans = -1;
 
-    while(low <= high)
+    while(low <= high)  // int arr[] = {3,4,4,7,8,10};       int n = 6, x= 5;
     {
         int mid = (low + high) / 2;
 
@@ -26,7 +26,7 @@ int findCeil(int arr[], int n, int x)
 {
     int low = 0, high = n-1, ans = -1;
 
-    while(low <= high)
+    while(low <= high)      // int arr[] = {3,4,4,7,8,10};       int n = 6, x= 5;
     {
         int mid = (low + high) / 2;
 
@@ -58,6 +58,68 @@ int main()
     return 0;
 }
 
+
+
+
+
+
+
+
+
+// java
+
+import java.util.Arrays;
+
+public class FloorCeilFinder {
+
+    public static int findFloor(int[] arr, int n, int x) {
+        int low = 0, high = n - 1, ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] <= x) {
+                ans = arr[mid];
+                // look for smaller index on right
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int findCeil(int[] arr, int n, int x) {
+        int low = 0, high = n - 1, ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= x) {
+                ans = arr[mid];
+                // look for larger index on left
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int[] getFloorAndCeil(int[] arr, int n, int x) {
+        int f = findFloor(arr, n, x);
+        int c = findCeil(arr, n, x);
+        return new int[]{f, c};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 4, 7, 8, 10};
+        int n = arr.length;
+        int x = 5;
+        int[] ans = getFloorAndCeil(arr, n, x);
+        System.out.println("The floor & Ceil are: " + ans[0] + " & " + ans[1]);
+    }
+}
 
 
 
