a9b2c21f8f6b6ec8d71e326fd85456c2fdd8fd62
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2024-01-18 18:54:21 +0530

    Java + C++

--- Strivers - Finding Max SubArray Sum Kadanes/Solution.cpp
+++ Strivers - Finding Max SubArray Sum Kadanes/Solution.cpp
@@ -1,5 +1,5 @@
 //https://takeuforward.org/data-structure/kadanes-algorithm-maximum-subarray-sum-in-an-array/
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int maxSubarraySum(int arr[], int n) 
 {
@@ -22,4 +22,27 @@ int main()
     int maxSum = maxSubarraySum(arr, n);
     cout << "The maximum subarray sum is: " << maxSum << endl;
     return 0;
+}
+
+///////////////////////////// JAVA //////////////////////////////////
+
+class Solution {
+    public static int maxSubarraySum(int arr[], int n) {
+        int maximum = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                maximum = Math.max(maximum, sum);
+            }
+        }
+        return maximum;
+    }
+  
+    public static void main(String args[]) {
+        int arr[] = { -1, 2, 5, -5, 3, 4, -9, 10, 5, -2 };
+        int n = arr.length;
+        int maxSum = maxSubarraySum(arr, n);
+        System.out.println("The max subarray sum is : " + maxSum);
+    }
 }
--- Strivers - Finding Missing num in Array/SOlution.cpp
+++ Strivers - Finding Missing num in Array/SOlution.cpp
@@ -20,3 +20,25 @@ int main()
     cout << "The missing number is: " << ans << endl;
     return 0;
 }
+
+
+///////////////////////////// JAVA //////////////////////////////////
+
+import java.util.*;
+class Solution {
+    public static int missingNumber(vector<Integer> a, int N) {
+        int arrSum = 0;
+        int totSum = (N * (N + 1)) / 2;
+        for (int i = 0; i < N; i++) {
+            arrSum += a.get(i);
+        }
+        return totSum - arrSum;
+    }
+  
+    public static void main(String args[]) {
+        Vector<Integer> a = new Vector<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 8, 9));
+        int n = a.size() + 1;
+        int missNum = missingNumber(a, n);
+        System.out.println("The missing number is: " + missNum);
+    }
+}
