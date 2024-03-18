2a198b450e121bd776321edf848bf80432effaf5
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2024-03-18 22:15:41 +0530

    s.cpp

--- Minimum Number of Arrows to Burst Balloons/s.cpp
+++ Minimum Number of Arrows to Burst Balloons/s.cpp
@@ -33,6 +33,62 @@ public:
     }
 };
 
+
+
+===========================================================================================
+
+
+
+
+
+// JAVA
+
+
+import java.util.Arrays;
+
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        if (points == null || points.length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1])); // sort by end points
+        
+        int arrows = 1; // initialize with one arrow for the first balloon
+        int end = points[0][1];
+        
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > end) {
+                // balloon cannot be burst by current arrow, shoot a new arrow
+                arrows++;
+                end = points[i][1];
+            }
+            // if the current balloon is overlapped with the previous, continue with the same arrow
+        }
+        
+        return arrows;
+    }
+}
+
+// int[][] points1 = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
+// System.out.println("Output for Example 1: " + new Solution().findMinArrowShots(points1));
+
+// int[][] points2 = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+// System.out.println("Output for Example 2: " + new Solution().findMinArrowShots(points2));
+
+// int[][] points3 = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
+// System.out.println("Output for Example 3: " + new Solution().findMinArrowShots(points3));
+
+
+
+
+
+
+======================================================================================================
+
+
+
+
 /*
 Consider a balloon with the point [xstart, xend]. In this case, a represents the first balloon [xstart1, xend1], 
 and b represents the second balloon [xstart2, xend2] when comparing them during the sorting process.
