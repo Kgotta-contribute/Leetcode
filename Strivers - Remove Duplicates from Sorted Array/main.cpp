19fdec8fbca0fee7d7ef6f33f7e513f6e386d8ca
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2024-04-02 23:05:08 +0530

    JAVA + Cpp

--- Strivers - Remove Duplicates from Sorted Array/main.cpp
+++ Strivers - Remove Duplicates from Sorted Array/main.cpp
@@ -24,3 +24,30 @@ int main() {
   }
 }
 
+==========================================
+
+JAVA 
+
+
+public class Main {
+    public static int removeDuplicates(int[] arr, int n) {
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 6};
+        int n = arr.length;
+        int k = removeDuplicates(arr, n);
+        System.out.println("The array after removing duplicate elements is ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
