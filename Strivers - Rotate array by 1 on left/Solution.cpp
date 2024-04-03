bc3451659c78b0ea64a027936ed4c988ccc31a1e
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2024-04-03 23:43:42 +0530

    Java + Cpp

--- Strivers - Rotate array by 1 on left/Solution.cpp
+++ Strivers - Rotate array by 1 on left/Solution.cpp
@@ -30,3 +30,39 @@ int main()
 	
 	return 0;
 }
+
+===========================================================
+
+JAVA 
+
+
+import java.util.Arrays;
+
+public class Main {
+  
+  public static void leftRotateArrayByOne(int[] arr, int n) {
+    int temp = arr[0];
+    for(int i = 0; i< n-1 ; i++) {
+      arr[i] = arr[i+1];
+    }
+    arr[n-1] = temp;
+  }
+  
+  public static void printArray(int[] arr, int n) {
+    for(int i=0; i<n ; i++) {
+      System.out.print(arr[i] + " ");
+    }
+  }
+  
+  public static void main(String[] args) {
+    int[] arr = {1,2,3,4,5,6,7,8,9,10};
+    int n = arr.length;
+    System.out.print("\nOriginal array is : ");
+    printArray(arr, n);
+
+    leftRotateArrayByOne(arr, n);
+
+    System.out.print("\nThe left Rotated array is : ");
+    printArray(arr, n);
+  }
+}
