e81f3de4e4311bc4e89bd1d2e090eae62ad80792
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2024-08-20 17:54:49 +0530

    Java + cpp + py

--- Strivers - Upper Bound/Solutions.cpp
+++ Strivers - Upper Bound/Solutions.cpp
@@ -1,36 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int upperBound(vector<int> &arr, int x, int n) 
-{
-    int low = 0, high = n - 1;
-    int ans = n;
-    while (low <= high) 
-    {
-        int mid = (low + high) / 2;
-        if (arr[mid] > x) 
-        {
-            ans = mid;
-            high = mid - 1;
-        }
-        else 
-        {
-            low = mid + 1;
-        }
-    }
-    return ans;
-}
-
-int main()
-{
-    vector<int> arr = {3, 5, 8, 9, 15, 19};
-    int n = 6, x = 9;
-    int ind = upperBound(arr, x, n);
-    cout << "The upper bound is the index: " << ind << "\n";
-    return 0;
-}
-
-
+// here we have to find smallest element greater than 'x' and then return its index !
 
 
 
@@ -65,6 +33,40 @@ public class Main {
 }
 
 
+
+// CPP
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n) 
+{
+    int low = 0, high = n - 1;
+    int ans = n;
+    while (low <= high) 
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] > x) 
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else 
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 9, 15, 19};
+    int n = 6, x = 9;
+    int ind = upperBound(arr, x, n);
+    cout << "The upper bound is the index: " << ind << "\n";
+    return 0;
+}
 
 
 
--- Strivers - Validate Binary Search Tree/Solutions.cpp
+++ Strivers - Validate Binary Search Tree/Solutions.cpp
@@ -1,3 +1,61 @@
+// JAVA 
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    private boolean isPossible(TreeNode root, long l, long r) {
+        if (root == null) return true;
+        if (root.val < r && root.val > l) {
+            return isPossible(root.left, l, root.val) && isPossible(root.right, root.val, r);
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        long min = -1000000000000L;
+        long max = 1000000000000L;
+        return isPossible(root, min, max);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Create the BST as given in the example
+        TreeNode root1 = new TreeNode(2);
+        root1.left = new TreeNode(1);
+        root1.right = new TreeNode(3);
+
+        Solution solution1 = new Solution();
+        boolean isValid1 = solution1.isValidBST(root1);
+        System.out.println("Example 1: " + (isValid1 ? "Valid BST" : "Not a valid BST"));
+
+        // Create another BST as given in the example
+        TreeNode root2 = new TreeNode(5);
+        root2.left = new TreeNode(1);
+        root2.right = new TreeNode(4);
+        root2.right.left = new TreeNode(3);
+        root2.right.right = new TreeNode(6);
+
+        Solution solution2 = new Solution();
+        boolean isValid2 = solution2.isValidBST(root2);
+        System.out.println("Example 2: " + (isValid2 ? "Valid BST" : "Not a valid BST"));
+    }
+}
+
+
+=============================================================================================
+
+// CPP
+
 #include <iostream>
 #include <limits>
 using namespace std;
@@ -54,63 +112,4 @@ int main() {
     cout << "Example 2: " << (isValid2 ? "Valid BST" : "Not a valid BST") << endl;
 
     return 0;
-}
-
-
-
-=============================================================================================
-
-
-
-// JAVA 
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
-class Solution {
-    private boolean isPossible(TreeNode root, long l, long r) {
-        if (root == null) return true;
-        if (root.val < r && root.val > l) {
-            return isPossible(root.left, l, root.val) && isPossible(root.right, root.val, r);
-        } else {
-            return false;
-        }
-    }
-
-    public boolean isValidBST(TreeNode root) {
-        long min = -1000000000000L;
-        long max = 1000000000000L;
-        return isPossible(root, min, max);
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        // Create the BST as given in the example
-        TreeNode root1 = new TreeNode(2);
-        root1.left = new TreeNode(1);
-        root1.right = new TreeNode(3);
-
-        Solution solution1 = new Solution();
-        boolean isValid1 = solution1.isValidBST(root1);
-        System.out.println("Example 1: " + (isValid1 ? "Valid BST" : "Not a valid BST"));
-
-        // Create another BST as given in the example
-        TreeNode root2 = new TreeNode(5);
-        root2.left = new TreeNode(1);
-        root2.right = new TreeNode(4);
-        root2.right.left = new TreeNode(3);
-        root2.right.right = new TreeNode(6);
-
-        Solution solution2 = new Solution();
-        boolean isValid2 = solution2.isValidBST(root2);
-        System.out.println("Example 2: " + (isValid2 ? "Valid BST" : "Not a valid BST"));
-    }
 }
